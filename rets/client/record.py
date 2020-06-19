28c52e3d559d0db9e0fc2dc5368ba34ebb36177a
Stephen Tse <stephen.tse@goproperly.com>
2020-06-19 15:22:03 -0400

    Adds primary key casing check.

--- rets/client/record.py
+++ rets/client/record.py
@@ -6,9 +6,15 @@ from rets.http import Object
 class Record:
 
     def __init__(self, resource_class, data: dict):
+        # Check if the reported primary key is in the data
+        if resource_class.resource.key_field in data:
+            self.resource_key = str(data[resource_class.resource.key_field])
+        else:
+            # If it is not, use the capitalized version. This is probably a hack.
+            self.resource_key = str(data[resource_class.resource.key_field.capitalize()])
+
         self.resource = resource_class.resource
         self.resource_class = resource_class
-        self.resource_key = str(data[resource_class.resource.key_field])
         self.data = data
 
     def get_objects(self, name: str, **kwargs) -> Sequence[Object]:
